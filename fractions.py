747bd9db10681da8c5caa5b4e5c58045a7094b95
Abe Lerman <abe893@gmail.com>
2017-05-21 19:10:37 -0400

    fractions

---
+++ fractions.py
@@ -0,0 +1,131 @@
+import operator
+
+def gcd(m, n):
+    def recurse(m, n):
+        if m % n == 0:
+            return n
+        else:
+            return recurse(n, m % n)
+    return recurse(m, n)
+
+
+class Fraction:   
+    
+    def __new__(cls, numerator, denomenator):
+        if numerator == 0:
+            return 0
+        else:
+            return super(Fraction, cls).__new__(cls)
+
+    def __init__(self, numerator, denomenator):
+        if not (isinstance(numerator, int) 
+                    and isinstance(denomenator, int)):
+            raise ValueError('Fraction constructors must both be integers')
+        
+        if denomenator == 0:
+            raise ValueError('Fraction denomenator must be non-zero int')
+
+        if denomenator < 0 and numerator >= 0:
+            numerator, denomenator = -1 * numerator, -1 * denomenator
+        
+        common = gcd(numerator, denomenator)
+        self.numerator = numerator // common
+        self.denomenator = denomenator // common
+
+    def __str__(self):
+        return "{} / {}".format(self.numerator, self.denomenator)
+    
+    def __repr__(self):
+        return self.__str__()
+
+    def _real(self):
+        return self.numerator / self.denomenator
+
+    def __neg__(self):
+        return - 1 * self
+
+    def __add__(self, other):
+        if isinstance(other, Fraction):
+            return Fraction(self.numerator * other.denomenator 
+                               + other.numerator * self.denomenator, 
+                            self.denomenator * other.denomenator)
+        elif isinstance(other, int):
+            if other == 0:
+                return self
+            else:
+                return self + Fraction(other, 1)
+        else:
+            raise ValueError('Must pass Fractions or Fraction & int')
+
+    def __radd__(self, other):
+        return self + other
+
+    def __sub__(self, other):
+        return self + -1 * other   
+
+    def __rsub__(self, other):
+        return other  + -1 * self 
+
+    def __mul__(self, other):
+        if isinstance(other, Fraction):
+            return Fraction(self.numerator * other.numerator, 
+                            self.denomenator * other.denomenator)    
+        elif isinstance(other, int):
+            if other == 0:
+                return 0
+            else:
+                return self * Fraction(other, 1)
+        else:
+            raise ValueError('Must pass Fractions or Fraction & int')
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __truediv__(self, other):
+        if isinstance(other, Fraction):
+            return self * Fraction(other.denomenator, other.numerator)  
+        elif isinstance(other, int):
+            if other == 0:
+                raise ValueError('Can\'t divide by zero')
+            else:
+                return self * Fraction(1, other)
+        else:
+            raise ValueError('Must pass Fractions or Fraction & int')
+
+    def __rtruediv__(self, other):
+        return Fraction(self.denomenator, self.numerator) * other
+
+    def _compare(op_func):
+        def operate(self, other):
+            if isinstance(other, Fraction):
+                return op_func(self.numerator 
+                                    * other.denomenator,
+                                other.numerator 
+                                    * self.denomenator)
+            else:
+                return op_func(self._real(), other)
+        return operate
+
+    @_compare
+    def __lt__(self, other):
+         return operator.lt(self, other)
+
+    @_compare
+    def __le__(self, other):
+        return operator.le(self, other)
+    
+    @_compare
+    def __eq__(self, other):
+        return operator.eq(self, other)
+
+    @_compare
+    def __ne__(self, other):
+        return operator.ne(self, other)
+
+    @_compare
+    def __gt__(self, other):
+        return operator.gt(self, other)
+
+    @_compare
+    def __ge__(self, other):
+        return operator.ge(self, other)
